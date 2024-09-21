a68c097b528b98a5e00fdd6261718737f6223ebc
Arhaan-Siddiquee <siddiqueearhaan@gmail.com>
2024-09-21 15:27:03 +0530

    initial commit

---
+++ carbon-footprint-tracker/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import CarbonForm from './components/CarbonForm';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <CarbonForm />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
